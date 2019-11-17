c5ffa15e5a1f315d5ca462aa9d85257d62f35f90
Ben Slater <ben@Bens-iMac.local>
2019-11-18 08:32:56 +1100

    Fix a broken elseif.. *yawn*

--- Source/HLEGraphics/BaseRenderer.h
+++ Source/HLEGraphics/BaseRenderer.h
@@ -32,7 +32,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #ifdef DAEDALUS_PSP
 #include <pspgu.h>
-#elseif DAEDALUS_VITA
+#elif DAEDALUS_VITA
 #include "SysVita/GL.h"
 #else
 #include "SysGL/GL.h"
